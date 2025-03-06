b081755ce09763e858e79e0f440b0ba8934f97dc
沈放歌个 <58197863+SFGG7@users.noreply.github.com>
2025-03-06 22:59:56 +0800

    迭代-增加字段

---
+++ src/EasyIotSharp.Core/Domain/Proejct/ProtocolConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyIotSharp.Core.Domain.Proejct
+{
+    /// <summary>
+    /// 协议配置表
+    /// </summary>
+    public class ProtocolConfig : BaseEntity<string>
+    {
+
+    }
+}
---
+++ src/EasyIotSharp.Core/GlobalEnums.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using UPrime.CodeAnnotations;
+
+namespace EasyIotSharp.Core
+{
+    /// <summary>
+    /// 合同主体
+    /// </summary>
+    public enum DataTypeMenu
+    {
+        /// <summary>
+        /// 全部(未知的)
+        /// </summary>
+        [EnumAlias("全部")]
+        None = 0,
+
+        /// <summary>
+        /// string
+        /// </summary>
+        [EnumAlias("string")]
+        String = 1,
+
+        /// <summary>
+        /// int
+        /// </summary>
+        [EnumAlias("int")]
+        Int = 2,
+
+        /// <summary>
+        /// double
+        /// </summary>
+        [EnumAlias("double")]
+        Double = 3,
+
+        /// <summary>
+        /// float
+        /// </summary>
+        [EnumAlias("float")]
+        Float = 4,
+
+        /// <summary>
+        /// bool
+        /// </summary>
+        [EnumAlias("bool")]
+        Bool = 2
+    }
+}
