f02a2928324ed3a0e648182ca8200ce751a796d8
82Xiaoyin <99532750+82Xiaoyin@users.noreply.github.com>
2025-03-10 17:15:40 +0800

    修改类型
    修改类型

--- src/EasyIotSharp.Core/Dto/Hardware/Params/QueryProtocolConfigByProtocolIdOutput.cs
+++ src/EasyIotSharp.Core/Dto/Hardware/Params/QueryProtocolConfigByProtocolIdOutput.cs
@@ -93,7 +93,7 @@ namespace EasyIotSharp.Core.Dto.Hardware.Params
         /// <summary>
         /// 验证数据类型不正确提示
         /// </summary>
-        public int ValidateMessage { get; set; }
+        public string ValidateMessage { get; set; }
         /// <summary>
         /// 排序
         /// </summary>
